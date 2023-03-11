cf0740c0a3b685b515b77b7fa1a1274e64336587
Marco Iván <morkiglogxx@gmail.com>
2023-03-10 19:09:07 -0600

    Input user

--- Assets/Scripts/InputUser.cs
+++ Assets/Scripts/InputUser.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
 using UnityEngine.UI;
 
 public class InputUser : MonoBehaviour
@@ -36,4 +37,26 @@ public class InputUser : MonoBehaviour
         //Do all the django stuff here
         Debug.Log("Enter Pressed");
     }
+
+    IEnumerator upload()
+    {
+        WWWForm form = new WWWForm();
+        form.AddField("listNumber", "2");
+        form.AddField("group", "1");
+
+        using (UnityWebRequest www = UnityWebRequest.Post("http;//40.76.234.232:8000/validacion-usuario/", form))
+        {
+            yield return www.SendWebRequest();
+            if(www.result != UnityWebRequest.Result.Success)
+            {
+                Debug.Log(www.error);
+            }
+            else
+            {
+                string txt = www.downloadHandler.text;
+                
+            }
+        }
+    }
+
 }
