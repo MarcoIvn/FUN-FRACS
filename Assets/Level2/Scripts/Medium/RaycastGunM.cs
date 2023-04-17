9359e3d4049f852deb6e6acbb7908114e04460a3
JosueFuentesR12 <105233426+JosueFuentesR12@users.noreply.github.com>
2023-04-16 19:02:52 -0500

    VenusTerminado
    Venus Esay, Medium y Hard terminados.

---
+++ Assets/Level2/Scripts/Medium/RaycastGunM.cs
@@ -0,0 +1,118 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(LineRenderer))]
+public class RaycastGunM : MonoBehaviour
+{
+    
+    public Camera playerCamera;
+    public Transform laserOrigin;
+    public float gunRange = 80f;
+    public float fireRate = 0.4f;
+    public float laserDuration = 3f;
+    public int asteroidsToDestroy; // Nuevo: cantidad de asteroides a destruir para ganar
+    public AudioSource audioLaser;
+    public AudioSource audioExplosion;
+
+    LineRenderer laserLine;
+    float fireTimer;
+    public int asteroidsDestroyed = 0; // Nuevo: contador de asteroides destruidos
+
+    //Win-Lose
+    private bool activated = false;
+
+    // Operaciones 
+    public int num1;
+    public int num2;
+    public string operation;
+
+    void Start()
+    {
+        GenerateMathProblem();
+    }
+
+    void Awake()
+    {
+        laserLine = GetComponent<LineRenderer>();
+    }
+    // Poner un nuevo asset, para que al entrar a el haga un check point y diga: has ganado, has perdido.
+    void Update()
+    {
+        fireTimer += Time.deltaTime;
+        if (Input.GetButtonDown("Fire1") && fireTimer > fireRate)
+        {
+            audioLaser.Play(); //laser
+            audioLaser.volume = 0.2f;
+            fireTimer = 0;
+            laserLine.SetPosition(0, laserOrigin.position);
+            Vector3 rayOring = playerCamera.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, 0));
+            RaycastHit hit;
+            if (Physics.Raycast(rayOring, playerCamera.transform.forward, out hit, gunRange))
+            {
+                laserLine.SetPosition(1, hit.point);
+                if (hit.transform.gameObject.CompareTag("Asteroid")) // Destruir asteroides
+                {
+                    Destroy(hit.transform.gameObject);
+                    asteroidsDestroyed++;
+                    audioExplosion.Play(); //explosion
+                    audioExplosion.volume = 1f;
+                }
+
+            }
+            else
+            {
+                laserLine.SetPosition(1, rayOring + (playerCamera.transform.forward * gunRange));
+            }
+            StartCoroutine(ShoorLaser());
+        }
+    }
+
+    public void GenerateMathProblem()
+    {
+        // Generate random numbers
+        //num1 = Random.Range(21, 41);
+        //num2 = Random.Range(1, 21);
+
+        // Generate random operation
+        int opIndex = Random.Range(0, 4);
+        switch (opIndex)
+        {
+            case 0:
+                num1 = Random.Range(21, 51);
+                num2 = Random.Range(1, 21);
+                operation = "+";
+                asteroidsToDestroy = num1 + num2;
+                break;
+            case 1:
+                num1 = Random.Range(21, 51);
+                num2 = Random.Range(1, 21);
+                operation = "-";
+                asteroidsToDestroy = num1 - num2;
+                break;
+            case 2:
+                num1 = Random.Range(1, 11);
+                num2 = Random.Range(1, 15);
+                operation = "*";
+                asteroidsToDestroy = num1 * num2;
+                break;
+            case 3:
+                num1 = Random.Range(11, 51);
+                num2 = Random.Range(1, 21);
+                operation = "/";
+                num1 *= num2; // multiply num1 to get a divisible number
+                asteroidsToDestroy = num1 / num2;
+                break;
+
+        }
+
+    }
+
+    IEnumerator ShoorLaser()
+    {
+        laserLine.enabled = true;
+        yield return new WaitForSeconds(laserDuration);
+        laserLine.enabled = false;
+    }
+    
+}
---
+++ Assets/Level2/Scripts/Medium/TriggerM.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TriggerM : MonoBehaviour
+{
+    
+    private bool activated = false;
+
+    public GameObject win;
+    public GameObject lose;
+
+    public RaycastGunM gunScript; // agregar una referencia a RaycastGun
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // asignar el valor de la instancia de RaycastGun a gunScript
+        gunScript = FindObjectOfType<RaycastGunM>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "source")
+        {
+            Debug.Log("Entraste a la nave nodrisa");
+            if (!activated)
+            {
+                activated = true;
+                if (gunScript.asteroidsDestroyed == gunScript.asteroidsToDestroy) // Nuevo: si se han destruido suficientes asteroides, imprimir "You Win" en la consola
+                {
+                    Debug.Log("Asteroids destruidos: " + gunScript.asteroidsDestroyed);
+                    Debug.Log("You Win");
+                    win.SetActive(true);
+                }
+                else  // "You lose" en la consola
+                {
+                    Debug.Log("Asteroids destruidos: " + gunScript.asteroidsDestroyed);
+                    Debug.Log("You lose");
+                    lose.SetActive(true);
+                }
+            }
+        }
+    }
+    
+}
