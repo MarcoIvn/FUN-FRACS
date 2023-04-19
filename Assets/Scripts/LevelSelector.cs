f7ba7093dd0f9db20ccc60c22a30d9cf1e4c7ed7
Marco Iván <morkiglogxx@gmail.com>
2023-04-19 13:50:10 -0600

    Avance Level 3

--- Assets/Scripts/LevelSelector.cs
+++ Assets/Scripts/LevelSelector.cs
@@ -175,9 +175,9 @@ public class LevelSelector : MonoBehaviour
             sceneToLoad = "Assets/Scenes/EarthLevels/" + "Earth" + levelChoice +".unity";
             SceneManager.LoadScene(sceneToLoad, LoadSceneMode.Single);
         }
-        else if (currentLevel.name == "Mercury2")
+        else if (currentLevel.name == "Venus2")
         {
-            sceneToLoad = "Assets/Scenes/MercuryLevels/" + "Mercury" + levelChoice + ".unity";
+            sceneToLoad = "Assets/Scenes/VenusLevels/" + "Venus" + levelChoice + ".unity";
             SceneManager.LoadScene(sceneToLoad, LoadSceneMode.Single);
         }
             
@@ -186,9 +186,9 @@ public class LevelSelector : MonoBehaviour
             sceneToLoad = "Assets/Scenes/MarsLevels/" + "Mars" + levelChoice + ".unity";
             SceneManager.LoadScene(sceneToLoad, LoadSceneMode.Single);
         }
-        else if (currentLevel.name == "Venus4")
+        else if (currentLevel.name == "Mercury4")
         {
-            sceneToLoad = "Assets/Scenes/VenusLevels/" + "Venus" + levelChoice + ".unity";
+            sceneToLoad = "Assets/Scenes/MercuryLevels/" + "Mercury" + levelChoice + ".unity";
             SceneManager.LoadScene(sceneToLoad, LoadSceneMode.Single);
         }
         else if (currentLevel.name == "Uran5")
